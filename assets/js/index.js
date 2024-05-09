d9a66cb764874a14c1f071a951ae1be8d6a16a28
chris kaba <chriskaba7@gmail.com>
2024-05-09 19:57:41 +0200

    HTML & CSS windowCreateContact v2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 
-let createBtn = document.querySelector('.header__left__creatContact')
+let createBtn = document.querySelector('.header__left__creatContact');
+let select = document.getElementById("select");
 
 function visibilityBtnCreateAction() {
     let creatAction = document.querySelector('.creatAction');
@@ -14,4 +15,35 @@ function name(params) {
     
 }
 
+
 createBtn.addEventListener('click',visibilityBtnCreateAction)
+
+// function changes() {
+
+//     let select_s = select.style;
+     
+//     switch(select.selectedIndex) {
+     
+//     case 0 :
+//     select_s.background = "url('./assets/images/Flag_of_the_Democratic_Republic_of_the_Congo.svg.png') no-repeat";
+//     break;
+     
+//     case 1 :
+//     select_s.background = "url('image2.ext') no-repeat";
+//     break;
+     
+//     case 2 :
+//     select_s.background = "url('image3.ext') no-repeat";
+//     break;
+     
+//     case 3 :
+//     select_s.background = "url('image4.ext') no-repeat";
+//     break;
+     
+//     default:
+//     select_s.background = "none";
+//     break;
+//     }
+// }
+
+// select.addEventListener('change',changes)
