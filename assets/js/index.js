2b75a940770bdbc629fa6d3d1714643785c343d5
chris kaba <chriskaba7@gmail.com>
2024-05-11 22:58:16 +0200

    HTML & CSS design v1 page create contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,3 +47,6 @@ createBtn.addEventListener('click',visibilityBtnCreateAction)
 // }
 
 // select.addEventListener('change',changes)
+
+//<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m6 9l6 6l6-6"/></svg>
+//<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m17.5 15.914l-5.5-5.5l-5.5 5.5L5.086 14.5L12 7.586l6.914 6.914z"/></svg>
