e9a53abc5a8e396b067d5f7c5fd838eceba656f9
Chris Kaba <chriskaba7@gmail.com>
2024-05-14 12:31:35 +0100

    add libelle v4

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,12 +39,38 @@ function hiddenModalAddLibelles() {
     windowDark.style.display = "none";
 }
 
+//<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84l3.96-5.58a.99.99 0 0 0 0-1.16z"/></svg>
+
+
+function createLibelle() {
+    
+    const libelles = document.querySelector('.header__left__libelles');
+    const libelleName = document.querySelector('#newLibelle');
+
+    const svg = document.createElement('img');
+    svg.scr = "./../images/libelle.svg"
+    svg.setAttribute('alt', "libelle icon")
+    
+    const span = document.createElement('span');
+    span.textContent = libelleName.value
+
+    
+    const libelleItem = document.createElement('div');
+    libelleItem.className = 'header__left__libelle__item';
+
+
+    libelleItem.append(svg, span);
+    libelles.appendChild(libelleItem);
+}
+
 
 ContainerCreateBtn.addEventListener('click',visibilityBtnCreateAction);
 oneContact.addEventListener('click',OpenWindowsCreatContact);
 contactBtn.addEventListener('click',OpenWindowsListContact);
 btnPlusLibelles.addEventListener('click',showModalAddLibelles);
 btnNothingLibelles.addEventListener('click',hiddenModalAddLibelles);
+saveLibelle.addEventListener('click',createLibelle)
+
 
 
 
